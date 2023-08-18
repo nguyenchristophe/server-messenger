6228e1a27ca0b5b33afb02c07b1a17e2c7406e05
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2023-08-19 00:30:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,15 @@ mongoose
     console.log("Error connecting to MongoDb", err);
   });
 
-app.listen(port, () => {
-  console.log("Server running on port 8000");
-});
+
+
+
+var server = app.listen(port || 3000,function() {
+  var port = server.address().port; 
+  console.log("Express is working on:"+port);
+
+
+})
 
 const User = require("./models/user");
 const Message = require("./models/message");
