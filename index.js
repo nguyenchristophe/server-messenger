16b37d139edc3356e8f83bf2b8af551c44363d38
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2023-08-19 00:23:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+const port = 80;
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect("mongodb+srv://christophe:Ovhcpp0503_&@cluster0.mw9gbe4.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Mongo Db");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb", err);
+  });
+
+app.listen(port, () => {
+  console.log("Server running on port 8000");
+});
